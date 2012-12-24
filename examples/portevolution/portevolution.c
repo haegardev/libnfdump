c15f5d2170ecc9dda7faa0354615998ddf783650
Gerard Wagener <haegardev@gmail.com>
2012-12-24 20:34:54 +0100

    Packed all global variables in a struct that is passed to each function

--- examples/portevolution/portevolution.c
+++ examples/portevolution/portevolution.c
@@ -38,47 +38,67 @@ typedef struct peer_s {
     //etc
 } peer_t;
 
-//TODO pass iport as command line option
-int iport = 25; /* Inspected port */
-int tas   = 0;  /* Target AS */
-int counter = 0; /* Number of processed flows */
-int matched = 0; /* Number of matched flows */
-GSList* srclist; /* List of IP addresses belonging to the AS denoted tas */
-int srcmembers = 0;
-int missedpeers = 0; /* Number of missed peers */
-int fullpeerlists = 0; /* Number of full peer lists */
+typedef struct portevolution_s{
+    int iport; /* Inspected port */
+    int tas;  /* Target AS */
+    int counter; /* Number of processed flows */
+    int matched; /* Number of matched flows */
+    GSList* srclist; /* List of IP addresses belonging to the AS denoted tas */
+    int srcmembers;
+    int missedpeers; /* Number of missed peers */
+    int fullpeerlists; /* Number of full peer lists */
+} portevolution_t;
 
-source_t* update_source_list(uint32_t ip);
-GSList* update_peer_list(source_t* src, uint32_t ip);
+//TODO set errno or something like that in the portevolution_t
+/* Functions */
+portevolution_t* initevolution(int iport, int tas);
+int process_record(portevolution_t* pe, master_record_t* r);
+void print_peer_scores(portevolution_t* pe, GSList* peers);
+void print_source_list(portevolution_t* pe);
+GSList* search_address(portevolution_t* pe, uint32_t ip);
+GSList* update_peer_list(portevolution_t* pe, source_t* src, uint32_t ip);
+source_t* update_source_list(portevolution_t* pe, uint32_t ip);
 
-int process_record(master_record_t* r)
+int process_record(portevolution_t* pe, master_record_t* r)
 {
     source_t* src;
   //FIXME does not take into account replies from the servers
     /* Test if host from AS tas connects to port iport in other AS */
-    if ((r->srcas == tas) && (r->dstport == iport)) {
-        src = update_source_list(r->v4.srcaddr);
+    if ((r->srcas == pe->tas) && (r->dstport == pe->iport)) {
+        src = update_source_list(pe, r->v4.srcaddr);
         if (src){
-            src->peers=update_peer_list(src,r->v4.dstaddr);
+            src->peers=update_peer_list(pe, src,r->v4.dstaddr);
         }else{
             /* The source list is full tell process_record to stop */
             return 0;    
         }
-        matched++;
+        pe->matched++;
     }
-    counter++;
+    pe->counter++;
     /* It is assumed that everything went fine for this record */
     return 1;
 }
 
 
-void init(void)
+portevolution_t* initevolution(int iport, int tas)
 {
-    srclist = NULL;
-
+    portevolution_t* pe;
+    pe = malloc(sizeof(portevolution_t));
+    if (pe){ 
+        pe->srclist = NULL;
+        pe->iport = iport; 
+        pe->tas   = tas;  
+        pe->counter = 0;
+        pe->matched = 0;
+        pe->srclist = NULL; 
+        pe->srcmembers = 0; 
+        pe->missedpeers = 0;
+        pe->fullpeerlists = 0;
+    }
+    return pe;
 }
 
-void print_peer_scores(GSList* peers)
+void print_peer_scores(portevolution_t* pe, GSList* peers)
 {
     GSList* item;
     peer_t* peer;
@@ -93,11 +113,11 @@ void print_peer_scores(GSList* peers)
     printf("\n");
 }
 
-void print_source_list(void)
+void print_source_list(portevolution_t* pe)
 {
     GSList* item; 
     source_t* src;
-    item = srclist;
+    item = pe->srclist;
     char as[40];
 
     while (item != NULL){
@@ -105,21 +125,21 @@ void print_source_list(void)
             src = (source_t*)item->data;
             /* Compute the number of full peer lists */
             if (src->peermembers == MAXPEERS) {
-                fullpeerlists++;
+                pe->fullpeerlists++;
             }
             src->ipv4addr = htonl(src->ipv4addr);
             inet_ntop(AF_INET, &src->ipv4addr, as, sizeof(as));
             printf("%s %d ", as,src->peermembers);
-            print_peer_scores(src->peers);
+            print_peer_scores(pe, src->peers);
             item = item->next; 
         }
     }
  }
 
-GSList* search_address(uint32_t ip)
+GSList* search_address(portevolution_t* pe, uint32_t ip)
 {
     GSList* item;
-    item = srclist;
+    item = pe->srclist;
     source_t *src;
     while (item) {
         if (item){
@@ -133,7 +153,7 @@ GSList* search_address(uint32_t ip)
     return NULL; 
 }
 
-GSList* update_peer_list(source_t* src, uint32_t ip)
+GSList* update_peer_list(portevolution_t* pe, source_t* src, uint32_t ip)
 {
     GSList* item;
     peer_t* peer;
@@ -165,7 +185,7 @@ GSList* update_peer_list(source_t* src, uint32_t ip)
             src->peermembers++;
         }
     } else {
-        missedpeers++;
+        pe->missedpeers++;
     } 
     /* If there is no memory or the peer list full no peer will be added 
      * and the old lits is returned 
@@ -176,24 +196,24 @@ GSList* update_peer_list(source_t* src, uint32_t ip)
 
 
 /* Update the source IP list and return a list of peers */
-source_t* update_source_list(uint32_t ip)
+source_t* update_source_list(portevolution_t* pe, uint32_t ip)
 {
     GSList *found;
     found = NULL;
     source_t* src;
-    found = search_address(ip);
+    found = search_address(pe, ip);
     if (found) {
         return (source_t*)found->data;
     }
     src = malloc(sizeof(source_t));
     if (src) {
-        if (srcmembers < MAXSOURCES) {
+        if (pe->srcmembers < MAXSOURCES) {
             src->ipv4addr = ip;
             src->peers = NULL;
             src->peermembers = 0;
-            srclist = g_slist_prepend(srclist,src);
-            srcmembers++;
-            return (source_t*)srclist->data;
+            pe->srclist = g_slist_prepend(pe->srclist,src);
+            pe->srcmembers++;
+            return (source_t*)(pe->srclist)->data;
         }
     } else{
         fprintf(stderr,"Cannot allocate memory");
@@ -206,14 +226,15 @@ int main (int argc, char* argv[])
 {
     libnfstates_t* states;
     master_record_t* r;
-    
+    portevolution_t* pe; 
     if (argc != 2) {
         fprintf(stderr,"An nfcapd file needs to be passed as command line argument\n");
         return (EXIT_FAILURE);
     }
     /* Initialize libnfdump */
     states = initlib(NULL, argv[1],NULL);
-    init();
+    /* Initialize port evolution */
+    pe = initevolution(25,0);
  
     if (states) {
         do {
@@ -223,28 +244,29 @@ int main (int argc, char* argv[])
                 if ( (r->flags & FLAG_IPV6_ADDR ) != 0 ) {
                     continue;
                 }
-                if (!process_record(r)) {
+                if (!process_record(pe, r)) {
                     break;
                 }
             }           
         } while (r);
 
-        print_source_list();
-        printf("#Inspected port: %d\n",iport);
-        printf("#Source AS number: %d\n",tas);
-        printf("#Processed records: %d\n",counter);
-        printf("#Matched records: %d\n",matched);
-        printf("#Source list members: %d\n",srcmembers);
-        if (srcmembers == MAXSOURCES){
+        print_source_list(pe);
+        printf("#Inspected port: %d\n",pe->iport);
+        printf("#Source AS number: %d\n",pe->tas);
+        printf("#Processed records: %d\n",pe->counter);
+        printf("#Matched records: %d\n",pe->matched);
+        printf("#Source list members: %d\n",pe->srcmembers);
+        if (pe->srcmembers == MAXSOURCES){
             printf("#Warning: Source list is truncated\n");
         }      
-        printf("#Number of missed peers: %d\n",missedpeers);
-        printf("#Number of full peer lists: %d\n", fullpeerlists);
+        printf("#Number of missed peers: %d\n",pe->missedpeers);
+        printf("#Number of full peer lists: %d\n", pe->fullpeerlists);
         printf("#Full peer list ratio: %.2f\n",
-               (float) fullpeerlists / (float) srcmembers); 
+               (float) pe->fullpeerlists / (float) pe->srcmembers); 
+
         /* Close the nfcapd file and free up internal states */
         libcleanup(states);
-        //TODO free up the used memory
+        //TODO free up memory
     }
     return(EXIT_SUCCESS);
 } 
