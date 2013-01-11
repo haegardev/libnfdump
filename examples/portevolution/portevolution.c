47a06e15f2381ba11dfdbecf3d56aeccf15cf8ec
Gerard Wagener <gerard.wagener@circl.lu>
2013-01-11 15:12:59 +0100

    Updated TODO list

--- examples/portevolution/portevolution.c
+++ examples/portevolution/portevolution.c
@@ -26,8 +26,7 @@
  *
  * TODO add support to handle more sourcelist such that more than 1 protocol 
  * canbe analyzed at the same time.
- * TODO export the data as json such that it can be sorted reprocessed via
- * other programming languages
+ * TODO export other fields aswell input, output
  * TODO Put these functions in a library
  */ 
 #include <stdio.h>
